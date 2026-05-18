24bb7d42a8dbbe2d8ae3262042cef77a7414d711
Chidananda1412 <chidananda1412@gmail.com>
2026-05-18 21:02:32 +0530

    worked on how to using rootdir in a clear way

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const homeRoutes = require('./routes/home');
 const productRoutes = require('./routes/products');
 const adminRoutes = require('./routes/admin');
+const rootDir = require('./views/util/path');
 
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +14,12 @@ app.use(homeRoutes);
 app.use('/admin/products', productRoutes);
 app.use('/admin', adminRoutes);
 
+app.use((req, res, next) => {
+    const errorPage = path.join(rootDir, 'views', 'PageNotFound.html');
+    res.status(404).sendFile(errorPage, err => {
+        if (err) next(err);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const rootDir = require('../views/util/path');
 
 router.get('/', (req, res, next) => {
-    const filePath = path.resolve(__dirname, '..', 'views', 'Home-page.html');
+    const filePath = path.join(rootDir, 'views', 'Home-page.html');
     res.sendFile(filePath, err => {
         if (err) return next(err);
     });
